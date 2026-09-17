aa7185e7584dee7c4b2995877fbffc28372de341
rendane <35165774+rendane@users.noreply.github.com>
2026-09-17 14:24:11 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-// استفاده از https به همراه نادیده گرفتن گواهی SSL خودامضا
+// استفاده از https برای پورت دارای SSL
 const TARGET_SERVER = 'https://172.245.180.219:2053';
 
 app.use('/', createProxyMiddleware({
     target: TARGET_SERVER,
     changeOrigin: true,
-    secure: false, // نادیده گرفتن گواهی SSL نامعتبر/خودامضا سرور
+    secure: false,
     ws: true,
     onError: (err, req, res) => {
         console.error('Proxy Error:', err.message);
-        res.status(502).send(`ارتباط با سرور برقرار نشد: ${err.message}`);
+        res.status(502).send(`خطا در برقراری ارتباط: ${err.message}`);
     }
 }));
 
