556780d79d8d611160111e42b3df85802522f091
rendane <35165774+rendane@users.noreply.github.com>
2026-09-17 15:12:56 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// استفاده از https برای پورت دارای SSL
+const TARGET_SERVER = 'https://172.245.180.219:2053';
+
+app.use('/', createProxyMiddleware({
+    target: TARGET_SERVER,
+    changeOrigin: true,
+    secure: false,
+    ws: true,
+    onError: (err, req, res) => {
+        console.error('Proxy Error:', err.message);
+        res.status(502).send(
+خطا در برقراری ارتباط: ${err.message}
+);
+    }
+}));
+
 // هدایت ترافیک کانکشن WebSocket به پورت اینباند
 app.use('/ws', createProxyMiddleware({
     target: 'https://172.245.180.219:2053',
